73fe33ea66bda492348c9423806ec352c8c2589b
jMarquesss <44167363+jMarquesss@users.noreply.github.com>
2018-11-17 22:41:48 +0000

    nono commit

--- src/test/java/BikeSharingTest.java
+++ src/test/java/BikeSharingTest.java
@@ -1,5 +1,4 @@
 import Exceptions.UserAlreadyExists;
-import Models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,34 +10,67 @@ class BikeSharingTest {
 
     @BeforeEach
     public void init() throws UserAlreadyExists{
-        b.registerUser(1,"testUser", 1);
+        b.registerUser(0, "testUser", 1);
     }
-    @Test
-    public void verifyCreditTest() throws UserAlreadyExists {
-        b.addCredit(1,5);
-        assertTrue(b.verifyCredit(1));
-    }
+
 
     @Test
     public void registerUserTestInvalid(){
        assertThrows(UserAlreadyExists.class,
-                ()->{b.registerUser(1,"testUser", 1);});
+               () -> {
+                   b.registerUser(0, "testUser", 1);
+               });
    }
 
 
     @Test
     public void registerUserTestValid()  throws UserAlreadyExists {
-        b.registerUser(0,"testUser0", 1);
-        assertTrue(b.getUsers().contains(new User(0,"testUser0", 1)));
+        b.registerUser(1, "testUser0", 1);
+        assertEquals(b.getUsers().get(1).getIDUser(), 1);
 
     }
 
     @Test
-
     public void addCreditTest(){
-
+        b.addCredit(0, 1);
+        assertEquals(b.getUsers().get(0).getCredit(), 1);
     }
 
 
+    /*
+    Não acontece nada quando falha
+     */
+    @Test
+    public void addCreditTestInvalidUser() {
+        b.addCredit(1, 1);
+        assertEquals(b.getUsers().get(0).getCredit(), 1);
+    }
+
+    /*
+    Não acontece nada quando a ammount e invalida
+     */
+    @Test
+    public void addCreditTestInvalidAmmount() {
+        b.addCredit(0, -1);
+        assertEquals(b.getUsers().get(0).getCredit(), -1);
+    }
+
+    @Test
+    public void verifyCreditTestValid() {
+        b.addCredit(0, 1);
+        assertTrue(b.verifyCredit(0));
+    }
+
+    @Test
+    public void verifyCreditTestInvalidUser() {
+        b.addCredit(1, 1);
+        assertFalse(b.verifyCredit(0));
+    }
+
+    @Test
+    public void verifyCreditTestInvalidCredit() {
+        b.addCredit(0, 0);
+        assertFalse(b.verifyCredit(0));
+    }
 
 }
