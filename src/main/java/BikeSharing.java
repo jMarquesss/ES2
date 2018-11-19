6723d9666ee0e6d20df342bd30054f366b65d84d
jMarquess <jp98-@hotmail.com>
2018-11-19 11:40:13 +0000

    decimo terceiro

--- src/main/java/BikeSharing.java
+++ src/main/java/BikeSharing.java
@@ -2,9 +2,6 @@ import Exceptions.UserAlreadyExists;
 
 public class BikeSharing{
     public static void main (String[] args) throws UserAlreadyExists {
-        BikeRentalSystem b = new BikeRentalSystem(1);
-        b.registerUser(1,"testUser", 1);
-        b.registerUser(2,"testUser2",2);
-        System.out.println("yolo");
+        System.out.println("Test");
     }
 }
--- src/test/java/BikeSharingTest.java
+++ src/test/java/BikeSharingTest.java
@@ -3,6 +3,7 @@ import Exceptions.UserDoesNotExists;
 import Models.Bike;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,8 +11,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class BikeSharingTest {
     BikeRentalSystem b = new BikeRentalSystem(1);
     Bike bici = new Bike(1);
+
     @BeforeEach
-    public void init() throws UserAlreadyExists{
+    public void setUp() throws UserAlreadyExists{
         b.registerUser(0, "testUser", 1);
     }
 
@@ -77,6 +79,113 @@ class BikeSharingTest {
         assertEquals(-1, b.getBicycle(1, 0, -1));
     }
 
+    //Test 1- returnBicycle
+    @Test
+    public void returnBicycleTestValid() throws UserDoesNotExists {
+        b.addBicycle(1, 1, 1);
+        b.addCredit(0, 1);
+        b.getBicycle(1,0,0);
+        float help=b.getUsers().get(0).getCredit();
+        assertFalse(b.returnBicycle(10,0,1)==help);
+    }
+
+    //Test 2- returnBicycle
+    @Test
+    public void returnBicycleTestInvalidUser() throws UserDoesNotExists {
+        b.addBicycle(1, 1, 1);
+        b.addCredit(0, 1);
+        b.getBicycle(1,0,0);
+        assertEquals(-1, b.returnBicycle(1,1,1));
+    }
+
+    //Test 3- returnBicycle
+    @Test
+    public void returnBicycleTestInvalidDeposit() throws UserDoesNotExists {
+        b.addBicycle(1, 1, 1);
+        b.addCredit(0, 1);
+        b.getBicycle(1, 0, 0);
+        assertEquals(-1, b.returnBicycle(0,1,1));
+    }
+
+    //Test 4- returnBicycle
+    @Test
+    public void returnBicycleTestInvalidEndTime() throws UserDoesNotExists {
+        b.addBicycle(1, 1, 1);
+        b.addCredit(0, 1);
+        b.getBicycle(1, 0, 0);
+        assertEquals(-1, b.returnBicycle(0,1,-1));
+    }
+
+    //Test 5- returnBicycle
+    @Test
+    public void returnBicycleTestInvalidBikeId() throws UserDoesNotExists {
+        b.addBicycle(1, 1, 1);
+        b.addCredit(0, 1);
+        b.getBicycle(1, 0, 0);
+        b.getBikes().get(0).setInUSe(false);
+        assertEquals(-1, b.returnBicycle(0,1,1));
+    }
+
+    /*Test 6- returnBicycle
+        Needs help
+    */
+    @Test
+    public void returnBicycleTestInvalidNoAvailableSpots() throws UserDoesNotExists {
+        b.addBicycle(1, 1, 1);
+        b.addCredit(0, 1);
+        b.getBicycle(1, 0, 0);
+
+        //assertEquals(-1, b.returnBicycle(0,1,1));
+    }
+
+    //Test 1- bicycleRentalFee
+    @Test
+    public void bicycleRentalFeeTestInvalidRentalProgram(){
+        assertEquals(0, b.bicycleRentalFee(0,0,1,0));
+    }
+
+    //Test 2- bicycleRentalFee
+    @Test
+    public void bicycleRentalFeeTestValidRentalProgram1(){
+        assertEquals((1-0)*b.getRentalFee(), b.bicycleRentalFee(1,0,1,0));
+    }
+
+    //Test 3- bicycleRentalFee
+    @Test
+    public void bicycleRentalFeeTestInvalidRentalProgram2(){
+        assertEquals(0, b.bicycleRentalFee(2,0,1,0));
+    }
+
+    //Test 4- bicycleRentalFee
+    @Test
+    public void bicycleRentalFeeTestValidRentalProgram2a(){
+        assertEquals((1-0)*b.getRentalFee(), b.bicycleRentalFee(2,0,1,1));
+    }
+
+    //Test 5- bicycleRentalFee
+    @Test
+    public void bicycleRentalFeeTestValidRentalProgram2b(){
+        assertEquals(b.getRentalFee()*10+((11-0)-10)*b.getRentalFee()/2, b.bicycleRentalFee(2,0,11,1));
+    }
+
+    //Test 6- bicycleRentalFee
+    @Test
+    public void bicycleRentalFeeTestInvalidNRentals(){
+        assertEquals(0, b.bicycleRentalFee(2,0,1,0));
+    }
+
+    //Test 7- bicycleRentalFee
+    @Test
+    public void bicycleRentalFeeTestInvalidStartAndEndTime(){
+        assertEquals(0, b.bicycleRentalFee(2,1,0,0));
+    }
+
+    //Test 8- bicycleRentalFee
+    @Test
+    public void bicycleRentalFeeTestInvalidRentalProgram3(){
+        assertEquals(0, b.bicycleRentalFee(3,0,1,0));
+    }
+
     //Test 1- verifyCredit
     @Test
     public void verifyCreditTestValid() {
@@ -112,7 +221,7 @@ class BikeSharingTest {
     @Test
     public void addCreditTestInvalidUser() {
         b.addCredit(1, 1);
-        assertEquals(1, b.getUsers().get(0).getCredit());
+        assertFalse(1== b.getUsers().get(0).getCredit());
     }
 
     /*
@@ -122,7 +231,7 @@ class BikeSharingTest {
     @Test
     public void addCreditTestInvalidAmmount() {
         b.addCredit(0, -1);
-        assertEquals(-1, b.getUsers().get(0).getCredit());
+        assertFalse(-1 == b.getUsers().get(0).getCredit());
     }
 
     //Test 1- registerUser
